5401e0124a0f460e61b4a672bdb002bee5462778
Wiilver <susareywvh@gmail.com>
2026-02-08 22:22:43 -0600

    Mal

--- react_7_buscaminas/src/Tabla.jsx
+++ react_7_buscaminas/src/Tabla.jsx
@@ -1,9 +1,30 @@
 import Fila from "./Fila.jsx"
+import {useState} from "react"
 
 function Tabla(){
+
+    const [tablero, setTablero] = useState(new Array(8).fill(new Array(8)));
+    
+    function hacer(){
+        
+        for(let i = 0; i < 8; i++) 
+        {
+            for(let j = 0; j < 8; j++) 
+            {
+                
+            }
+        }
+        
+    }
+
+    function imprimir(){
+        hacer();
+        console.log(tablero);
+    }
+
     return(
         <table>
-            <tbody>
+            <tbody onClick={imprimir}>
                 <Fila/>
                 <Fila/>
                 <Fila/>
