6b4fb6fc903e0a65b7fcee886541e9aa054f711b
Wiilver <susareywvh@gmail.com>
2026-02-10 23:16:41 -0600

    yaesta

--- react_7_buscaminas/src/Tabla.jsx
+++ react_7_buscaminas/src/Tabla.jsx
@@ -3,22 +3,17 @@ import {useState} from "react"
 
 function Tabla(){
 
-    const [tablero, setTablero] = useState(new Array(8).fill(new Array(8)));
-    
+    const [tablero, setTablero] = useState(()=>(hacer));
+
     function hacer(){
-        
-        for(let i = 0; i < 8; i++) 
-        {
-            for(let j = 0; j < 8; j++) 
-            {
-                
-            }
-        }
-        
+        let list = [];
+        let arr = [];
+        for(let i = 0; i < 8; i++) arr.append("-");
+        for(let i = 0; i < 8; i++) list.append(arr);
+        return arr;
     }
 
     function imprimir(){
-        hacer();
         console.log(tablero);
     }
 
